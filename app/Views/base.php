213f6ce16d340b0ced447738b56134b8d2b0263d
DeVitor <97829726+DeVitorCruz@users.noreply.github.com>
2026-01-21 19:13:02 -0300

    View structure.

--- app/Views/base.php
+++ app/Views/base.php
@@ -21,59 +21,233 @@
     <!-- noUiSider JS -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.5.1/nouislider.min.js"></script>
 
+    <!-- Font awesome 6.1.2 -->
+    <script src="https://kit.fontawesome.com/5f0a932aad.js" crossorigin="anonymous"></script>
+
+    <style>
+        .cart-hidde {
+            display: none;
+        }
+
+        @media only screen and (max-width: 991px) {
+            .free-shipping-text {
+                display: none !important;
+            }
+
+            .cart-hidde {
+                display: flex;
+            }
+        }
+    </style>
+
 
 </head>
 
 <body>
-    <header>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-                <a href="<?= base_url('/') ?>" class="navbar-brand">My E-Commerce</a>
-                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <?php if (isset($isLoginPage) && $isLoginPage): ?>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a href="<?= base_url('auth/login'); ?>" class="nav-link">Login</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="<?= base_url('auth/cadastre') ?>" class="nav-link">Register</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="<?= base_url('/') ?>" class="nav-link">Home</a>
-                            </li>
-                        </ul>
+    <header>
+
+        <?php if ($this->renderSection('announcementBar')): ?>
+            <?= $this->renderSection('announcementBar') ?>
+        <?php endif; ?>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="container-fluid">
+                        <div class="col-md-12">
+                            <a href="<?= base_url('/') ?>" class="navbar-brand">
+                                <img src="https://new-ella-demo.myshopify.com/cdn/shop/files/ella-logo-black-compressor.png?v=1629858814&width=300" alt="" width="90" height="41" class="d-inline-block align-text-top">
+                            </a>
+                        </div>
                     </div>
-                <?php else: ?>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a href="<?= base_url('auth/login'); ?>" class="nav-link">Login</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="<?= base_url('/') ?>" class="nav-link">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="<?= base_url('product') ?>" class="nav-link">Product List</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="<?= base_url('product/create') ?>" class="nav-link">Add Product</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a role="button" href="#" class="nav-link dropdown-toggle" id="settingsDropdown" data-bs-toggle="dropdown" aria-expanded="false">Settings</a>
-                                <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
-                                    <li><a href="<?= base_url('settings/description') ?>" class="dropdown-item">Description</a></li>
-                                    <li><a href="<?= base_url('settings/assign_attributes') ?>" class="dropdown-item">Assign Attributes</a></li>
-                                </ul>
-                            </li>
-                        </ul>
+                </div>
+                <div class="row">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="container-fluid">
+                                <div class="collapse navbar-collapse justify-content-end">
+                                    <div class="col-md-4">
+                                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 d-flex justify-content-end">
+                                            <li class="nav-item">
+                                                <span class="fs-6 text nav-link">Customer Service 091 234-ELLA</span>
+                                            </li>
+                                            <li class="nav-item dropdown">
+                                                <a class="fs-6 text nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    END
+                                                </a>
+                                                <ul class="dropdown-menu">
+                                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                    <li>
+                                                        <hr class="dropdown-divider">
+                                                    </li>
+                                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                                </ul>
+                                            </li>
+                                            <li class="fs-6 text nav-item dropdown">
+                                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                                    USD
+                                                </a>
+                                                <ul class="dropdown-menu">
+                                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                                    <li>
+                                                        <hr class="dropdown-divider">
+                                                    </li>
+                                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                                </ul>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-md-2">
+                                        <form class="d-flex" role="search">
+                                            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                            <button class="btn btn-outline-success" type="submit">Search</button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                <?php endif; ?>
+                    <div class="row d-flex justify-content-end">
+                        <div class="col-md-6">
+                            <div class="container-fluid">
+                                <h6 class="free-shipping-text d-flex justify-content-end">Free shipping on All Orders. No Minimum Purchase</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="cart-hidde col-md-6 col-sm-6">
+                            <div class="container-fluid">
+                                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar_main-content" aria-controls="navbar_main-content" aria-expanded="false" aria-label="Toggle navigation">
+                                    <i class="fa-solid fa-user"></i>
+                                </button>
+                                <div class="collapse navbar-collapse justify-content-end" id="navbar_main-content">
+                                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-right: 0px !important;">
+                                        <?php if (isset($isLoginPage) && $isLoginPage): ?>
+                                            <li class="nav-item">
+                                                <a href="<?= base_url('/') ?>" class="nav-link">Home</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="<?= base_url('auth/login'); ?>" class="nav-link">Login</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="<?= base_url('auth/register') ?>" class="nav-link">Register</a>
+                                            </li>
+                                        <?php else: ?>
+                                            <?php if (session()->get('user_id')): ?>
+                                                <li class="nav-item dropdown">
+                                                    <a role="button" href="#" class="nav-link dropdown-toggle" id="settingsDropdown" data-bs-toggle="dropdown" aria-expanded="false">My profile</a>
+                                                    <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
+                                                        <li class="list-group-item"><a href="<?= base_url('/profile') ?>">Edit Profile</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('orders/history') ?>">Order History</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('wishlist') ?>">Wishlist</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('profile/updatePassword') ?>">Change Password</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('logout') ?>">Logout</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="nav-item dropdown">
+                                                    <a role="button" href="#" class="nav-link dropdown-toggle" id="settingsDropdown" data-bs-toggle="dropdown" aria-expanded="false">Settings</a>
+                                                    <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
+                                                        <li><a href="<?= base_url('settings/description') ?>" class="dropdown-item">Description</a></li>
+                                                        <li><a href="<?= base_url('settings/assign_attributes') ?>" class="dropdown-item">Assign Attributes</a></li>
+                                                    </ul>
+                                                </li>
+                                            <?php else: ?>
+                                                <li class="nav-item">
+                                                    <a href="<?= base_url('/') ?>" class="nav-link">Shopping Cart</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="<?= base_url('/') ?>" class="nav-link">My favorite</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="<?= base_url('auth/login'); ?>" class="nav-link">Sign in or Create an account</a>
+                                                </li>
+                                            <?php endif; ?>
+                                        <?php endif; ?>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="container-fluid">
+                                <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar_main-content" aria-controls="navbar_main-content" aria-expanded="false" aria-label="Toggle navigation">
+                                    <i class="fa-solid fa-user"></i>
+                                </button>
+                                <div class="collapse navbar-collapse justify-content-end" id="navbar_main-content">
+                                    <ul class="navbar-nav me-auto mb-2 mb-lg-0" style="margin-right: 0px !important;">
+                                        <?php if (isset($isLoginPage) && $isLoginPage): ?>
+                                            <li class="nav-item">
+                                                <a href="<?= base_url('/') ?>" class="nav-link">Home</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="<?= base_url('auth/login'); ?>" class="nav-link">Login</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a href="<?= base_url('auth/register') ?>" class="nav-link">Register</a>
+                                            </li>
+                                        <?php else: ?>
+                                            <?php if (session()->get('user_id')): ?>
+                                                <li class="nav-item dropdown">
+                                                    <a role="button" href="#" class="nav-link dropdown-toggle" id="settingsDropdown" data-bs-toggle="dropdown" aria-expanded="false" style="color: black;">My profile</a>
+                                                    <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
+                                                        <li class="list-group-item"><a href="<?= base_url('/profile') ?>">Edit Profile</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('profile/dashboard') ?>">General Manager</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('orders/history') ?>">Order History</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('wishlist') ?>">Wishlist</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('profile/updatePassword') ?>">Change Password</a></li>
+                                                        <li class="list-group-item"><a href="<?= base_url('logout') ?>">Logout</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="nav-item dropdown">
+                                                    <a role="button" href="#" class="nav-link dropdown-toggle" id="settingsDropdown" data-bs-toggle="dropdown" aria-expanded="false" style="color: black;">Settings</a>
+                                                    <ul class="dropdown-menu" aria-labelledby="settingsDropdown">
+                                                        <li><a href="<?= base_url('settings/description') ?>" class="dropdown-item">Description</a></li>
+                                                        <li><a href="<?= base_url('settings/assign_attributes') ?>" class="dropdown-item">Assign Attributes</a></li>
+                                                    </ul>
+                                                </li>
+                                            <?php else: ?>
+                                                <li class="nav-item">
+                                                    <a href="<?= base_url('/') ?>" class="nav-link">Shopping Cart</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="<?= base_url('/') ?>" class="nav-link">My favorite</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a href="<?= base_url('auth/login'); ?>" class="nav-link">Sign in or Create an account</a>
+                                                </li>
+                                            <?php endif; ?>
+                                        <?php endif; ?>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </nav>
+        <div class="row bg-black">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link text-white active" aria-current="page" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+        </div>
+
+        <?php if ($this->renderSection('banner') !== null): ?>
+            <?= $this->renderSection('banner'); ?>
+        <?php endif; ?>
+
     </header>
 
     <main class="container">
--- app/Views/index.php
+++ app/Views/index.php
@@ -4,23 +4,170 @@
 Home
 <?= $this->endSection() ?>
 
+<?= $this->section('announcementBar') ?>
+
+<style>
+    .announcement-bar {
+        background-color: #f8d7da;
+        color: #721c24;
+        padding: 10px 0;
+        overflow: hidden;
+        position: relative;
+        width: 100%;
+        white-space: nowrap;
+        border-bottom: 1px solid #f5c6cb;
+    }
+
+    .announcement-content {
+        display: inline-block;
+        padding-left: 100%;
+        animation: scrollLeftToRight 20s linear infinite;
+    }
+
+    .announcement-item {
+        margin-right: 50px;
+        display: inline-block;
+        font-size: 16px;
+        font-weight: bold;
+    }
+
+    @keyframes scrollLeftToRight {
+        0% {
+            transform: translateX(100%);
+        }
+
+        100% {
+            transform: translateX(-100%);
+        }
+    }
+</style>
+
+<nav class="nav">
+    <div class="announcement-bar">
+        <div class="announcement-content">
+            <?php foreach ($announces as $announce): ?>
+                <span class="announcement-item"><?= $announce['message']; ?></span>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</nav>
+
+<?= $this->endSection() ?>
+
+<?= $this->section('banner') ?>
+
+<style>
+    .carousel-item img {
+        height: 600px;
+        object-fit: cover;
+    }
+
+    .carousel-caption {
+        position: absolute;
+        top: 56%;
+        transform: translateY(-50%);
+        left: 15%;
+        text-align: center;
+    }
+
+    .carousel-caption h2 {
+        font-size: 3rem;
+        font-weight: bold;
+    }
+
+    .carousel-captopn p {
+        font-size: 1.25rem;
+    }
+
+    @media only screen and (max-width: 768px) {
+        .carousel-captio h2 {
+            font-size: 2rem;
+        }
+
+        .carousel-caption p {
+            font-size: 1rem;
+        }
+
+        .carousel-caption a {
+            font-size: .9rem;
+        }
+    }
+</style>
+
+<div id="bannerCarousel" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#bannerCarousel" data-bs-slide-to="0" aria-label="Slide 1" aria-current="true" class="active"></button>
+        <button type="button" data-bs-target="#bannerCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#bannerCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+
+    <div class="carousel-inner">
+
+        <div class="carousel-item active">
+            <img src="https://images.pexels.com/photos/28680370/pexels-photo-28680370/free-photo-of-cozy-coffee-cup-and-backgammon-on-persian-rug.jpeg" alt="Banner 1" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Featured Products</h5>
+                <p>Discover our best-selling products and exclusive offers.</p>
+                <a href="#" class="btn btn-primary">Shop Now</a>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <img src="https://images.pexels.com/photos/28818953/pexels-photo-28818953/free-photo-of-charming-cafe-exterior-with-vintage-signage.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Banner 2" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>New Arrivals</h5>
+                <p>Check out the latest trends in fashion and accessories.</p>
+                <a href="#" class="btn btn-primary">Explore Collection</a>
+            </div>
+        </div>
+
+        <div class="carousel-item">
+            <img src="https://images.pexels.com/photos/27916602/pexels-photo-27916602/free-photo-of-a-man-on-a-motorcycle-is-sitting-in-the-middle-of-a-street.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Banner 3" class="d-block w-100">
+            <div class="carousel-caption d-none d-md-block">
+                <h5>Special Deals</h5>
+                <p>Exclusive discounts on selected products for a limited time.</p>
+                <a href="#" class="btn btn-primary">Get Discount</a>
+            </div>
+        </div>
+
+        <button type="button" class="carousel-control-prev" data-bs-target="#bannerCarousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+
+        <button type="button" class="carousel-control-next" data-bs-target="#bannerCarousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
+
 <?= $this->section('content') ?>
 
 <div class="container mt-5">
     <h1 class="text-center">Welcome to Our Ecommerce Store</h1>
     <p class="text-center">Explore our amazing products!</p>
-
     <div class="row">
-        <div class="col-md-6 container mt-5">
-            <div class="text-center">
-                <a href="<?= base_url('product/create') ?>" class="btn btn-primary btn-lg">Cadastrate a Product</a>
+        <?php foreach ($products as $product): ?>
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <h5 class="card-title"><?= esc($product['name']) ?></h5>
+                        <p class="card-text"><?= esc($product['description']) ?></p>
+                        <p class="card-text">Price: $<?= esc($product['price']) ?></p>
+                        <p class="card-text">Stock: <?= esc($product['stock']) ?></p>
+                        <form action="<?= base_url('cart/add') ?>" method="POST">
+                            <input type="hidden" name="product_id" id="product_id" value="<?= $product['product_id'] ?>">
+                            <input type="hidden" name="price" id="price" value="<?= esc($product['price']) ?>">
+                            <input type="number" name="quantity" id="quantity" value="1" min="1" max="<?= $product['stock'] ?>">
+                            <button type="submit" class="btn btn-primary">Add to Cart</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-        </div>
-        <div class="col-md-6 container mt-5">
-            <div class="text-center">
-                <a href="<?= base_url('product/') ?>" class="btn btn-success btn-lg">List of Product</a>
-            </div>
-        </div>
+        <?php endforeach; ?>
     </div>
 </div>
 
